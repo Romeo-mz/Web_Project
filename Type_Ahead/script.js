d4fa97fa77227bfb9efcbaaab3a238dbc8ed37de
Romeo-mz <romeo_martinez@etu.u-bourgogne.fr>
2023-02-15 09:33:09 +0100

    Need to improve time loading

--- Type_Ahead/script.js
+++ Type_Ahead/script.js
@@ -1,1 +1,32 @@
 const endpoint = 'https://raw.githubusercontent.com/high54/Communes-France-JSON/master/france.json';
+
+const cities = [];
+
+fetch(endpoint)
+.then(garb => garb.json())
+.then(data => cities.push(...data)); // ... use to spread data into an array
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+
+        const regex = new RegExp(wordToMatch, 'gi'); //gi , global and i for uppercase and minuscase
+        return place.Nom_commune.match(regex);
+    })
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value,cities);
+    const html = matchArray.map(place => {
+        return `
+        <li>
+            <span class='name'>${place.Nom_commune}</span>
+            <span class='postal-code'>${place.Code_postal}</span>
+        </li>
+        `
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('keyup', displayMatches);
