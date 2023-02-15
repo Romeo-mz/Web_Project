ed40f8a11a8ce6a824f32f45c6ec79a895508cce
Romeo-mz <romeo_martinez@etu.u-bourgogne.fr>
2023-02-15 14:31:02 +0100

    Finished script, need to improve keyup event

--- Type_Ahead/script.js
+++ Type_Ahead/script.js
@@ -7,26 +7,38 @@ fetch(endpoint)
 .then(data => cities.push(...data)); // ... use to spread data into an array
 
 function findMatches(wordToMatch, cities){
-    return cities.filter(place => {
+    if(wordToMatch !== ''){
+        return cities.filter(place => {
 
-        const regex = new RegExp(wordToMatch, 'gi'); //gi , global and i for uppercase and minuscase
-        return place.Nom_commune.match(regex);
-    })
+            const regex = new RegExp(wordToMatch, 'gi'); //gi , global and i for uppercase and minuscase
+            return place.Nom_commune.match(regex);
+        })
+    }
 }
 
 function displayMatches(){
     const matchArray = findMatches(this.value,cities);
     const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.Nom_commune.replace(regex,`<span class=hl>${this.value}</span>`)
         return `
         <li>
-            <span class='name'>${place.Nom_commune}</span>
+            <span class='name'>${cityName}</span>
             <span class='postal-code'>${place.Code_postal}</span>
         </li>
         `
     }).join('');
     suggestions.innerHTML = html;
 }
+/*
+let timeoutId;
 
+function onSearchInput() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => displayMatches(), 200);
+}
+*/
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
-searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('change', displayMatches);
+//searchInput.addEventListener('keyup',onSearchInput);
